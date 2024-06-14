3a0a757a4372b7fdd31a1312a26a83d1906d8098
mehdi pourhasan <mprhasn@gmail.com>
2024-06-14 14:17:55 +0330

    add webhooks for create delete and user update

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,13 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "./shared.typs";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
 export async function getUserById(params: any) {
   try {
@@ -11,6 +18,64 @@ export async function getUserById(params: any) {
     const user = await User.findOne({ clerkId: userId });
 
     return user;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+
+    await User.findOneAndUpdate({ clerkId }, updateData, { new: true });
+
+    revalidatePath(path);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId } = params;
+
+    const user = await User.findOneAndDelete({ clerkId });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // Delete user from database (questions, answers, comments, etc.)
+
+    const userQuestionIds = await Question.find({ author: user._id }).distinct(
+      "_id"
+    );
+
+    await Question.deleteMany({ author: user._id });
+
+    // TODO delete user answer, comments, etc.
+
+    const deleteUser = await User.findByIdAndDelete(user._id);
+
+    return deleteUser;
   } catch (err) {
     console.error(err);
     throw err;
